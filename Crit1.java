6befeceba7a39c9da38955701045889de2651882
fktschoolstuff <fred.thao@csuglobal.edu>
2024-06-15 21:48:11 -0400

    Add files via upload
    critical thinking week 1

---
+++ Crit1.java
@@ -0,0 +1,18 @@
+
+public class Crit1 {
+	public static void main(String[] args) {
+		// Defining and initializing variables
+		String firstName = "John";
+		String lastName = "Smith";
+		String streetAddress = "4311 Last Road";
+		String city = "Abyssmal";
+		String zipcode = "43110";
+		
+		// Printing variables on separate lines
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(streetAddress);
+		System.out.println(city);
+		System.out.println(zipcode);
+	}
+}
